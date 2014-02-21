f90da4b3e3f7608223bcdcdb2b3076f7a345cde9
Julia Evans <julia@jvns.ca>
2014-02-20 18:50:42 -0800

    Update fix-my-mailman.js

--- fix-my-mailman.js
+++ fix-my-mailman.js
@@ -2,4 +2,4 @@ var text = $('pre')[0].innerText.substr(0, 100);
 text = text.replace(/ \w+$/, '').replace(/\s+/, " ");
 var search = '"' + text + '"' + ' site:comments.gmane.org';
 console.log(search);
-window.location = "http://duckduckgo.com/?q=! " + search;
+window.location = "http://duckduckgo.com/?q=!g " + search;
