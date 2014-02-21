88a79027212911e3a2273fff263eef3f5e5f9568
Julia Evans <julia@jvns.ca>
2014-02-20 18:59:34 -0800

    Oops, forgot the search?

--- fix-my-mailman.js
+++ fix-my-mailman.js
@@ -2,4 +2,4 @@ var text = $('pre')[0].innerText.substr(0, 100);
 text = text.replace(/ \w+$/, '').replace(/\s+/, " ");
 var search = '"' + text + '"' + ' site:comments.gmane.org';
 search = search.replace(' ', '+');
-window.location = "http://google.com/?hl=en&output=search&sclient=psy-ab&q=" + search + "&btnI=1";
+window.location = "http://google.com/search?hl=en&output=search&sclient=psy-ab&q=" + search + "&btnI=1";
