6c7f6c5e9d8b44869ea92e1159c6e385e307bc51
Julia Evans <julia@jvns.ca>
2014-02-20 19:01:46 -0800

    Update fix-my-mailman.js

--- fix-my-mailman.js
+++ fix-my-mailman.js
@@ -1,5 +1,5 @@
 var text = $('pre')[0].innerText.substr(0, 100);
-text = text.replace(/ \w+$/, '').replace(/\s+/, " ");
+text = text.replace(/ \w+$/, '').replace(/\s+/, " ").replace("\n", " ");
 var search = '"' + text + '"' + ' site:comments.gmane.org';
 search = search.replace(' ', '+');
 window.location = "http://google.com/search?hl=en&output=search&sclient=psy-ab&q=" + search + "&btnI=1";
