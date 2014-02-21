6d93862500a68b1cb5ba5f940232ff433226e8a9
Julia Evans <julia@jvns.ca>
2014-02-20 18:58:20 -0800

    Try to make my search query string better

--- fix-my-mailman.js
+++ fix-my-mailman.js
@@ -1,4 +1,5 @@
 var text = $('pre')[0].innerText.substr(0, 100);
 text = text.replace(/ \w+$/, '').replace(/\s+/, " ");
 var search = '"' + text + '"' + ' site:comments.gmane.org';
-window.location = "http://google.com/?hl=en&q= " + search + "&btnI=1";
+search = search.replace(' ', '+');
+window.location = "http://google.com/?hl=en&output=search&sclient=psy-ab&q=" + search + "&btnI=1";
